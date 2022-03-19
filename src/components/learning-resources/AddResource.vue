68cbb5aedf02583858e0f2cd4400a729e0a1991e
Ayman Mohamed <aymantaha632@gmail.com>
2022-03-19 01:44:25 -0400

    add resource form
    adding and styling the form

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,9 +1,62 @@
 <template>
-  <h2>add resource</h2>
+  <base-card>
+    <form>
+      <div class="form-control">
+        <label for="name">Title:</label>
+        <input type="text" name="name" id="name" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description: </label>
+        <textarea
+          type="text"
+          name="description"
+          id="description"
+          rows="3"
+        ></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link: </label>
+        <input type="url" name="link" id="link" />
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+  </base-card>
 </template>
 
 <script>
-export default {};
+import BaseButton from '../ui/BaseButton.vue';
+import BaseCard from '../ui/BaseCard.vue';
+export default {
+  components: { BaseCard, BaseButton },
+};
 </script>
 
-<style></style>
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
