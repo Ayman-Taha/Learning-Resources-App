faddf5af8a75bc1bfdb93f2a62ff4a61d266b2c3
Ayman Mohamed <aymantaha632@gmail.com>
2022-03-19 00:51:24 -0400

    adding base button comp
    implementing, styling and adding the base button comp

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -3,8 +3,8 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
+        <base-button mode="flat">Delete</base-button>
       </header>
-      <button>Delete</button>
       <p>{{ description }}</p>
       <nav><a :href="link">View Resource</a></nav>
     </base-card>
